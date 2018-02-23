95ad8ee57f3fc7f6ec8ed4926e61b7fb77fdfb8e
SanaFrj <sanaferjani95@gmail.com>
2018-02-23 09:38:35 +0100

    second commit

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { RouterModule, Routes } from '@angular/router';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./app.component";
+
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: [],
+  bootstrap: []
+})
+export class AppRoutingModule { }
